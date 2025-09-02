0d8d9f23d7ae4aa18e68cf646f53d983bd87d224
Christina Trochidou <xristinatrox@yahoo.gr>
2025-09-02 11:06:21 +0300

    Create 16x16 grid dynamically with JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.createElement("div");
+container.className ="grid-container";
+document.body.appendChild(container);
+
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.className = "grid-square";
+    container.appendChild(square);
+}
