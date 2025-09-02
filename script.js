8f86569d73c0a9a7a12a9a5e1d0e9a9d6d4aa3fe
Christina Trochidou <xristinatrox@yahoo.gr>
2025-09-02 13:29:42 +0300

    Add interactive New Grid button with user-defined grid size

--- script.js
+++ script.js
@@ -19,6 +19,31 @@ for (let i = 0; i < 256; i++) {
    btn.textContent = "New Grid";
    document.body.insertBefore(btn, container);
    btn.className = "grid-button";
-   
+   btn.addEventListener("click", () => {
+   let squaresPerSide = prompt("Enter number of squares per side(max 100:");
+   squaresPerSide = Number(squaresPerSide);
+   if ( squaresPerSide > 100) squaresPerSide = 100;
+   createGrid(squaresPerSide);
+});
+   function createGrid(squaresPerSide) {
+    container.innerHTML = "";
+    const totalPixels = 640;
+    const squareSize = totalPixels / squaresPerSide;
+
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+        const square = document.createElement("div");
+        square.className = "grid-square";
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        container.appendChild(square);
+    }
+
+    const squares = document.querySelectorAll('.grid-square');
+    squares.forEach(square => {
+        square.addEventListener("mouseenter", () => {
+           square.style.backgroundColor = "black"; 
+        });
+    });
+   }
 
 
