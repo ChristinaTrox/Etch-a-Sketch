fda7c15120fc614e013763120a75d95d6e19f8eb
Christina Trochidou <xristinatrox@yahoo.gr>
2025-11-24 11:53:30 +0200

    I made the brush img changing the color or erasing the existing colours by clicking it

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
+let mode = "rainbow";
+
 const container = document.createElement("div");
 container.className ="grid-container";
 document.body.appendChild(container);
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.className = "grid-square";
-    container.appendChild(square);
-}
- const squares = document.querySelectorAll('.grid-square');
-     
-     squares.forEach(square => {
-     square.addEventListener("mouseenter", () => {
-   const r = Math.floor(Math.random() * 256);
-   const g = Math.floor(Math.random() * 256);
-   const b = Math.floor(Math.random() * 256);
-   const randomColor = `rgb(${r}, ${g}, ${b})`;
-    square.style.backgroundColor = randomColor;
-   });
-}); 
-   
+createGrid(16);
 
    const btn = document.createElement("button");
    btn.textContent = "Choose your squares";
-   document.body.insertBefore(btn, container);
+  
    btn.className = "grid-button";
+
    btn.addEventListener("click", () => {
    let squaresPerSide = prompt("Enter number of squares per side(max 100:");
    if (squaresPerSide === null) return;
@@ -31,6 +18,41 @@ for (let i = 0; i < 256; i++) {
    if ( squaresPerSide > 100) squaresPerSide = 100;
    createGrid(squaresPerSide);
 });
+
+
+const resetBtn = document.createElement("button");
+resetBtn.textContent = "Reset Sketch";
+resetBtn.className = "grid-button";
+
+
+resetBtn.addEventListener("click", () =>  createGrid(16));
+
+
+const btnContainer = document.createElement("div");
+btnContainer.className = "button-container";
+document.body.insertBefore(btnContainer, container);
+btnContainer.appendChild(btn);
+btnContainer.appendChild(resetBtn);
+
+const toolsDiv = document.querySelector(".tools");
+const mainWrapper = document.createElement("div");
+mainWrapper.className = "main-wrapper";
+document.body.appendChild(mainWrapper);
+mainWrapper.appendChild(toolsDiv);
+mainWrapper.appendChild(container);
+
+
+
+const brushImg = document.querySelector(".brush");
+brushImg.addEventListener("click", () => {
+    if (mode === "rainbow") mode = "black";
+    else if(mode === "black") mode = "erase";
+    else mode = "rainbow";
+
+    brushImg.style.transform = "scale(1.1)";
+    setTimeout(() => brushImg.style.transform = "scale(1)", 150);
+});
+
    function createGrid(squaresPerSide) {
     container.innerHTML = "";
     const totalPixels = 640;
@@ -41,34 +63,32 @@ for (let i = 0; i < 256; i++) {
         square.className = "grid-square";
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
+
+        square.addEventListener("mouseenter", ()=> {
+    if (mode === "rainbow") {
+        const r = Math.floor(Math.random()* 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    } else if (mode === "black") {
+        square.style.backgroundColor = "black";
+    } else if (mode === "erase"){
+        square.style.backgroundColor = "#eee";
+        }
+});
+
         container.appendChild(square);
     }
 
-    const squares = document.querySelectorAll('.grid-square');
-    squares.forEach(square => {
-        square.addEventListener("mouseenter", () => {
-   const r = Math.floor(Math.random() * 256);
-   const g = Math.floor(Math.random() * 256);
-   const b = Math.floor(Math.random() * 256);
-   const randomColor = `rgb(${r}, ${g}, ${b})`;
-    square.style.backgroundColor = randomColor;
-   });
-        });
     }
+
+
+ 
+
+
+
    
-const resetBtn = document.createElement("button");
-resetBtn.textContent = "Reset Sketch";
-resetBtn.className = "grid-button";
-document.body.insertBefore(resetBtn, container);
 
-resetBtn.addEventListener("click", () => {
-    container.innerHTML = "";
-    createGrid(16);
-});
 
-const btnContainer = document.createElement("div");
-btnContainer.className = "button-container";
-document.body.insertBefore(btnContainer, container);
-btnContainer.appendChild(btn);
-btnContainer.appendChild(resetBtn);
+
 
